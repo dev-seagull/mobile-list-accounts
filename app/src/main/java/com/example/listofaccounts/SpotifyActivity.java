36be16d09138b8af540371c2be0235b53a5fbede
Kasra Alizadeh <k.alizadeh1997@gmail.com>
2022-08-21 12:59:32 +0430

    Activity pages were added

---
+++ app/src/main/java/com/example/listofaccounts/SpotifyActivity.java
@@ -0,0 +1,13 @@
+package com.example.listofaccounts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpotifyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spotify);
+    }
+}
