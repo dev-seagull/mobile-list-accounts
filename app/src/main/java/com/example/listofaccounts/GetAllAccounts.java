1d9d327d11abc8946203b015b389c7d1b5ffce65
Kasra Alizadeh <k.alizadeh1997@gmail.com>
2022-08-30 19:27:35 +0430

    Add clean code step

--- app/src/main/java/com/example/listofaccounts/GetAllAccounts.java
+++ app/src/main/java/com/example/listofaccounts/GetAllAccounts.java
@@ -8,14 +8,14 @@ import android.content.Intent;
 import java.util.ArrayList;
 
 public class GetAllAccounts{
-    public static ArrayList<String> getAccountsOfDevice(String string , Activity activity){
-        Intent intent = AccountManager.newChooseAccountIntent(null, null, new String[]{"com.google"},
+    public static ArrayList<String> getAccountsOfDevice(String type , Activity activity){
+        Intent intent = AccountManager.newChooseAccountIntent(null, null, new String[]{type},
                 false, null, null, null, null);
         activity.startActivityForResult(intent, 23);
 
 
         AccountManager accountManager = AccountManager.get(activity.getApplicationContext());
-        Account[] accounts = accountManager.getAccountsByType(string);
+        Account[] accounts = accountManager.getAccountsByType(type);
         ArrayList<String> listOfAccounts = new ArrayList<>();
 
         for(Account account: accounts){
